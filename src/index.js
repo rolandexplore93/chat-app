892b4c7eee71757cd9f7ce0601f11c11de565f30
rolandexplore93 <roland2rule@gmail.com>
2022-07-30 13:25:07 +0100

    Setup socket.io to enable connection between the server and client side

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const path = require('path');
+const http =  require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, (req, res) => {
+io.on('connection', () => {
+    console.log("New Web Socket connection")
+})
+
+server.listen(port, (req, res) => {
     console.log(`Listening to the server at port ${port}`)
 })
