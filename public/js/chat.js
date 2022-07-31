7844dc46f5a29f92f826a9af871b55745cd16466
rolandexplore93 <roland2rule@gmail.com>
2022-07-31 09:54:15 +0100

    use socket.io to send messages between clients

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,12 @@
-io()
+const socket = io();
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#submission').addEventListener('submit', (e) => {
+    e.preventDefault()
+    // const message = document.querySelector('input').value;
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message)
+})
--- src/index.js
+++ src/index.js
@@ -11,10 +11,50 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
-    console.log("New Web Socket connection")
+let greeting = {
+    message: "Welcome Anonymous!",
+    no: 250
+}
+
+io.on('connection', (socket) => {
+
+    socket.emit('message', greeting)
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// io.on('connection', (socket) => {
+//     console.log("New Web Socket connection")
+//     // socket.emit('countUpdated', count)
+
+//     // socket.on('increment', () => {
+//     //     count++
+//     //     console.log(count)
+
+//     //     io.emit('countUpdated', count) // io.emit emits to all connections
+//     // })
+// })
 
 server.listen(port, (req, res) => {
     console.log(`Listening to the server at port ${port}`)
-})
+});
