04b327afe5b4016616f085c422f36de5d18f9861
rolandexplore93 <roland2rule@gmail.com>
2022-08-01 17:31:48 +0100

    Modify form and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,19 +1,31 @@
 const socket = io();
+const $messageForm = document.querySelector('#submission');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#my-location')
 
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#submission').addEventListener('submit', (e) => {
-    e.preventDefault()
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    $messageFormButton.setAttribute('disabled', 'disabled');
+    
     const message = e.target.elements.message.value
     socket.emit('sendMessage', message, (serverResponse) => {
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus()
+        
         if (serverResponse) return console.log(serverResponse)
     })
 })
 
-document.querySelector('#my-location').addEventListener('click', (e) => {
-    e.preventDefault()
+$sendLocationButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    $sendLocationButton.setAttribute('disabled', 'disabled')
     if (!navigator.geolocation){
         return alert('Geolocation is not supported by your browser!')
     }
@@ -25,7 +37,8 @@ document.querySelector('#my-location').addEventListener('click', (e) => {
         };
 
         socket.emit('sendLocation', sendLocation, (serverResponse) => {
-            console.log(serverResponse)
+            console.log(serverResponse);
+            $sendLocationButton.removeAttribute('disabled');
         })
     })
 })
